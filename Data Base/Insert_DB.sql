7419a8b8b6b671acac217aff6670925ba991b19c
Tijame98 <154690297+Tijame98@users.noreply.github.com>
2024-04-14 18:04:38 +0200

    Create Insert_DB.sql

---
+++ Data Base/Insert_DB.sql
@@ -0,0 +1,25 @@
+INSERT INTO eleve values ('500415610577', 'Mark', 'marks');
+
+INSERT INTO eleve values('50041561999', 'Anas', 'reda');
+
+INSERT INTO eleve values('50041562256', 'Reda', 'anas');
+
+INSERT INTO eleve values('50041562252', 'Anas', 'jasser');
+
+INSERT INTO eleve values('50041564555', 'Jasser', 'reda');
+
+INSERT INTO photo values (2, '500415610577', 'photo_01.jpg');
+
+INSERT INTO photo values (3, '50041561999', 'photo_2.jpg');
+
+INSERT INTO photo values (4, '50041562256', 'photo_7.jpg');
+
+INSERT INTO photo values (5, '50041562252');
+
+INSERT INTO cours values('CSED', 'Reseaux de petri');
+
+INSERT INTO cours values('IA', 'pekpe');
+
+INSERT INTO promo values('SE-4', 'Syst enmbarq 4');
+
+INSERT INTO salle values('A207', 'IA', 'SE-4');
