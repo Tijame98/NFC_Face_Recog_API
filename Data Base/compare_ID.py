126cb96d8e0bc847bd49ae0a9b1967c03cb50437
Tijame98 <154690297+Tijame98@users.noreply.github.com>
2024-04-14 18:10:00 +0200

    Create compare_ID.py

---
+++ Data Base/compare_ID.py
@@ -0,0 +1,24 @@
+import psycopg2 as pg
+import RC522/extract_ID as ext
+
+#se connecter a la base de donner locale sur la raspi 3
+conn = pg.connect(dbname = 'nfc', user= 'raspi', password = 'admin', host = 'localhost')
+
+cur = conn.cursor()
+
+#chercher si le ID scanner est bien dans l base de données 
+cur.execute("SELECT * FROM eleve")
+
+#stoker la sortie du requte dans une liste pour faire un retour si le test est passé ou pas
+Val = cur.fetchone()
+
+with open('feedback.txt', 'w') as f:
+    if Val[0] == ext.id_value:
+        f.write('ID is OK' + str(Val[1]) + str(Val[2]))
+    else:
+        f.write('No matching ID')
+
+f.close()
+
+cur.close()
+conn.close()
