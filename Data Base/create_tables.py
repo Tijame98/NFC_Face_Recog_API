69a72951326cf75bff1d10c674132ea727240b41
Tijame98 <154690297+Tijame98@users.noreply.github.com>
2024-04-14 17:59:49 +0200

    Update create_tables.py

--- Data Base/create_tables.py
+++ Data Base/create_tables.py
@@ -1,1 +1,16 @@
+import psycopg2 as pg
 
+with open('schema.sql', 'r') as f:
+	schema = f.read()
+ 
+conn = pg.connect(dbname = 'nfc', user = 'raspi', password = 'admin', host = 'localhost')
+if(conn):
+	print("Connectd to database")	
+else:
+	print("Connexion Failed")
+cur = conn.cursor()
+cur.execute(schema)
+conn.commit()
+print('Tables are created')
+cur.close()
+conn.close()
