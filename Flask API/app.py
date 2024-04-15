51691ef0d8b7140b1f56fad635dc8a6a0fda8d05
Tijame98 <154690297+Tijame98@users.noreply.github.com>
2024-04-15 21:40:12 +0200

    Update app.py

--- Flask API/app.py
+++ Flask API/app.py
@@ -1,11 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+# Route pour la page d'accueil
 @app.route('/')
 def index():
-    return render_template('Homepage.html')
+    return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# Route pour la section Étudiant
+@app.route('/etudiant')
+def etudiant():
+    return render_template('etudiant.html')
+
+# Route pour la section Professeur
+@app.route('/professeur')
+def professeur():
+    return render_template('professeur.html')
+
+# Route pour la section Administration
+@app.route('/administration')
+def administration():
+    return render_template('administration.html')
+
 
