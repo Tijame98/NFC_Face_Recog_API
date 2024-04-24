64e324929d33217d48d93709bac441627fc3c962
Tijame98 <154690297+Tijame98@users.noreply.github.com>
2024-04-24 17:12:22 +0200

    Update config.py

--- Flask API/config.py
+++ Flask API/config.py
@@ -3,4 +3,3 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = ' postgres://raspi:mkIWGPH0eGtRMApDnwV5Pu1frBGfgq1U@dpg-cocsnu63e1ms739lgb80-a.frankfurt-postgres.render.com/nfc_r0a3'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'your_secret_key'
