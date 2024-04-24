13c7810775d767e0f81207b18f5b403e1c71b7c1
Tijame98 <154690297+Tijame98@users.noreply.github.com>
2024-04-24 17:04:14 +0200

    Update app.py

--- Flask API/app.py
+++ Flask API/app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
 
 # Route pour la page d'accueil
 @app.route('/')
